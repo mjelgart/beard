83e467c40eaf6c1640f870d13fa78fb4ffe39860
Michael Elgart <mje@michaelelgart.com>
2020-05-03 22:23:17 -0500

    now working on actual beard logic

--- src/App.js
+++ src/App.js
@@ -1,44 +1,20 @@
 import React, { useState } from 'react';
 
 
-const getPrice = (setPrice) => {
-  fetch('https://api.coindesk.com/v1/bpi/currentprice/USD.json')
-        .then(res => res.json())
-        .then((data) => {
-          setPrice(data.bpi.USD.rate)
-        })
-        .catch(console.log)
-
-}
-
-const getActivity = (setActivity) => {
-  fetch('https://www.boredapi.com/api/activity')
-  .then(res => res.json())
-  .then((data) => {
-    setActivity(data.activity)
-  })
-  .catch(console.log)
-}
 
 function App() {
-  const [price, setPrice] = useState(0)
-  const [activity, setActivity] = useState("Go Fishing")
+  const [units, setUnits] = useState("mm")
+
+  const date = new Date()
+  const month = date.toLocaleString('default', {month: 'long'})
+  const beardLength = date.toLocaleString('default', {month: 'short'})
 
   return (
     <div>
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">Bitcoin Price</h5>
-          <button onClick={() => getPrice(setPrice)}>Refresh Price</button>
-          <p class="card-text">${price}</p>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Bored API</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Get an Idea for an Activity:</h6>
-          <button onClick={() => getActivity(setActivity)}>New Idea</button>
-          <p class="card-text">{activity}</p>
+          <h5 class="card-title">Beard Length</h5>
+          <p class="card-text">This month is {month}. Beard length is {beardLength} {units}</p>
         </div>
       </div>
     </div>
